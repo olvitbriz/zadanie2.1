a264c7e63789f6e4ffdbad61081b42ad744c77ed
Olia Brizgina <olvitbriz@gmail.com>
2023-04-16 12:52:30 +0400

    build successful

--- src/test/java/ru/netology/FormTest.java
+++ src/test/java/ru/netology/FormTest.java
@@ -40,12 +40,12 @@ public class FormTest {
     @Test
    public void positiveTest() {
 
-        driver.findElement(By.cssSelector("[data-test-id=name] input)")).sendKeys("Ольга");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input)")).sendKeys("+79603445206");
-        driver.findElement(By.cssSelector("[data-test-id=agreement])")).click();
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Ольга");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79603445206");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.cssSelector("button.button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success])")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals(expected, actual);
     }
 }
